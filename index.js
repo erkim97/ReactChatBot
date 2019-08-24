d7b1eb0ea395365035afeb47cc84f56613cff12b
Sehyung Kim <kimeric97@hotmail.com>
2019-08-24 14:50:33 -0700

    Changes port error commit

--- index.js
+++ index.js
@@ -2,8 +2,9 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send({'hello': 'there'});
+   res.send({'hi': 'there'})
 });
 
-const PORT = process.env.port || 5000;
-app.listen({PORT});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT);s
