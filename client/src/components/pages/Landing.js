d76234b4e894174eecfde80d71f3ee1c5b0b7045
Sehyung Kim <kimeric97@hotmail.com>
2019-09-09 01:40:28 -0700

    Recommit

--- client/src/components/pages/Landing.js
+++ client/src/components/pages/Landing.js
@@ -4,7 +4,7 @@ const Landing = () => {
     return (
         <div style={{ textAlign: 'center' }}><h1>Ask for jokes or questions!</h1>
             <p>my responses, given to you, with the help of the chatbot</p>
-            <img src="https://miro.medium.com/max/5250/1*m9IJdAYW04MYh75ivpwUNA.png" alt="Chatbot robot image" style={{ height: 400, width: 600, float: "left", border: '1px solid lightgray' }}></img></div>
+            <img src="https://miro.medium.com/max/5250/1*m9IJdAYW04MYh75ivpwUNA.png" alt="Chatbot robot image" style={{ height: 400, width: 600, float: "left", border: '1px solid lightgray', marginTop: 15 }}></img></div >
     )
 }
 
