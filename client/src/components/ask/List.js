a0a08a139fefa76ba9521afe14dca67ea1db048a
Sehyung Kim <kimeric97@hotmail.com>
2019-09-09 01:31:41 -0700

    text change

--- client/src/components/ask/List.js
+++ client/src/components/ask/List.js
@@ -8,7 +8,7 @@ const List = () => {
             <li>-Birthday, my favorite </li>
             <li>-Recommendations on food (Only 3 options thus far- Ramen, Sushi, General restaurants)</li>
             <li>-The meaning of life</li>
-            <li>-Can send your information to the mongoDB to recommend more ramen places(WIP)</li>
+            <li>-You can input and send your information to the mongoDB when asking for ramen</li>
         </ul>
     )
 };
