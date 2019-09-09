11e8a5ac25919122a42fd534ddd47ea30e122490
Sehyung Kim <kimeric97@hotmail.com>
2019-09-09 01:17:13 -0700

    restyle image

--- client/src/components/pages/Landing.js
+++ client/src/components/pages/Landing.js
@@ -3,8 +3,8 @@ import React from 'react';
 const Landing = () => {
     return (
         <div style={{ textAlign: 'center' }}><h1>Ask for jokes or questions!</h1>
-            my responses, given to you, with the help of the chatbot
-            <img src="https://miro.medium.com/max/5250/1*m9IJdAYW04MYh75ivpwUNA.png" alt="Chatbot robot image" style={{ minHeight: 500, maxHeight: 500, width: 400, position: 'absolute', textAlign: "left", border: '1px solid lightgray' }}></img></div>
+            <h3>my responses, given to you, with the help of the chatbot</h3>
+            <img src="https://miro.medium.com/max/5250/1*m9IJdAYW04MYh75ivpwUNA.png" alt="Chatbot robot image" style={{ height: 400, width: 600, float: "left", border: '1px solid lightgray' }}></img></div>
     )
 }
 
