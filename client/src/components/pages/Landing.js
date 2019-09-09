229a9cf60327920675a792a35b46000266265cb9
Sehyung Kim <kimeric97@hotmail.com>
2019-09-09 01:22:32 -0700

    landing page <p> tag change

--- client/src/components/pages/Landing.js
+++ client/src/components/pages/Landing.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Landing = () => {
     return (
         <div style={{ textAlign: 'center' }}><h1>Ask for jokes or questions!</h1>
-            <h3>my responses, given to you, with the help of the chatbot</h3>
+            <p>my responses, given to you, with the help of the chatbot</p>
             <img src="https://miro.medium.com/max/5250/1*m9IJdAYW04MYh75ivpwUNA.png" alt="Chatbot robot image" style={{ height: 400, width: 600, float: "left", border: '1px solid lightgray' }}></img></div>
     )
 }
