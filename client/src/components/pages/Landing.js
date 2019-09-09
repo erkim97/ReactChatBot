49b4a5f311b6c22f0ef73d4b48ed4c9f0362ad86
Sehyung Kim <kimeric97@hotmail.com>
2019-09-09 00:53:01 -0700

    change heading on aboutchatbot.js, added image on landing.js

--- client/src/components/pages/Landing.js
+++ client/src/components/pages/Landing.js
@@ -3,7 +3,8 @@ import React from 'react';
 const Landing = () => {
     return (
         <div style={{ textAlign: 'center' }}><h1>Ask for jokes or questions!</h1>
-            my responses, given to you, with the help of the chatbot</div>
+            my responses, given to you, with the help of the chatbot
+            <img src="https://miro.medium.com/max/5250/1*m9IJdAYW04MYh75ivpwUNA.png" alt="Chatbot robot image"></img></div>
     )
 }
 
