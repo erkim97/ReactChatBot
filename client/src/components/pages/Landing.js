27a68d143a5ee0fd8d3b4c5261c979edfa285fab
Sehyung Kim <kimeric97@hotmail.com>
2019-09-09 01:03:06 -0700

    image resizing

--- client/src/components/pages/Landing.js
+++ client/src/components/pages/Landing.js
@@ -4,7 +4,7 @@ const Landing = () => {
     return (
         <div style={{ textAlign: 'center' }}><h1>Ask for jokes or questions!</h1>
             my responses, given to you, with the help of the chatbot
-            <img src="https://miro.medium.com/max/5250/1*m9IJdAYW04MYh75ivpwUNA.png" alt="Chatbot robot image" style={{ minHeight: 500, maxHeight: 500, width: 400, position: 'absolute', textAlign: "center", border: '1px solid lightgray' }}></img></div>
+            <img src="https://miro.medium.com/max/5250/1*m9IJdAYW04MYh75ivpwUNA.png" alt="Chatbot robot image" style={{ minHeight: 500, maxHeight: 500, width: 400, position: 'absolute', textAlign: "left", border: '1px solid lightgray' }}></img></div>
     )
 }
 
