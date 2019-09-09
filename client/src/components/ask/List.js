579cbb2a6e20a08918c1d33344935b7fcca21ea8
Sehyung Kim <kimeric97@hotmail.com>
2019-09-09 00:33:25 -0700

    List changed to ul tags

--- client/src/components/ask/List.js
+++ client/src/components/ask/List.js
@@ -2,14 +2,14 @@ import React from 'react';
 
 const List = () => {
     return (
-        <div>
-            -Ask for a joke
-            -Any general small talk questions
-            -Birthday
-            -Recommendations on food (Only 3 options thus far- Ramen, Sushi, General restaurants)
-            -The meaning of life
-            -Can send your information to the mongoDB to recommend more ramen places(WIP)
-        </div>
+        <ul>
+            <li>-Ask for a joke</li>
+            <li>-Any general small talk questions</li>
+            <li>-Birthday</li>
+            <li>-Recommendations on food (Only 3 options thus far- Ramen, Sushi, General restaurants)</li>
+            <li>-The meaning of life</li>
+            <li>-Can send your information to the mongoDB to recommend more ramen places(WIP)</li>
+        </ul>
     )
 };
 
